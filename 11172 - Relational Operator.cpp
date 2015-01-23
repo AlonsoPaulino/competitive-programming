a7a578aef70d1883954b39b4ebf8d8fb48ea8744
Luis Alonso <alonsopf1@gmail.com>
2015-01-22 23:48:41 -0500

    Update 11172 - Relational Operator.cpp

--- 11172 - Relational Operator.cpp
+++ 11172 - Relational Operator.cpp
@@ -1,24 +1,16 @@
 #include<iostream>
 using namespace std;
-int main ()
-{
+int main (){
         int testCases;
         int num1 , num2;
  
         cin>>testCases;
        
-        for(int i=0; i<testCases ; i++)
-        {
+        for(int i=0; i<testCases ; i++){
                 cin>>num1>>num2;
-                if(num1 > num2){
-                        cout<<">"<<endl;
-                }
-                else if(num1 < num2){
-                        cout<<"<"<<endl;
-                }
-                else {
-                        cout<<"="<<endl;
-                }
+                if(num1 > num2) cout<<">"<<endl;
+                else if(num1 < num2)cout<<"<"<<endl;
+                else cout<<"="<<endl;
         }
-        return 0;
+    return 0;
 }
