27a2d45b705e132b7770df1b178e6663b306a86e
Luis Alonso Paulino Flores <alonsopf1@hotmail.com>
2015-08-31 00:03:04 -0500

    edit code bear and poker

--- Codeforces/Bear and Poker.cpp
+++ Codeforces/Bear and Poker.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
-#define MAX 100001
 using namespace std;
 
-int n, a, v[MAX], val, aux;
+int n, a, val, aux;
 bool ans;
 
 int main(){
 	while ( cin >> n ){
 		ans = true;
 		for ( int i = 0; i < n; ++i ){
-			cin >> v[i];			
+			cin >> a;			
 			if(ans){
 				if( i == 0 ){
-					val = v[i];
+					val = a;
 					while( val % 3 == 0 || val % 2 == 0){
 						if(val % 3 == 0) val/=3;
 						else if(val % 2 == 0) val/=2;
 					}
 				}else{
-					aux = v[i];
+					aux = a;
 					while( aux % 3 == 0|| aux % 2 == 0){
 						if(aux % 3 == 0) aux/=3;
 						else if(aux % 2 == 0) aux/=2;
