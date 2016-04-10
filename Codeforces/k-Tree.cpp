eeaa91ff8414e29b173b1e8aedcf5e80ffb43ce5
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2016-04-10 14:24:14 -0500

    k-tree solving

--- Codeforces/k-Tree.cpp
+++ Codeforces/k-Tree.cpp
@@ -1,36 +1,54 @@
 #include <bits/stdc++.h>
-#define MAX 101
+#define pi acos(-1)
+#define sz(x) (int) x.size()
+#define pii pair<int, int>
+#define mii map<int, int>
+#define vi vector<int>
+#define mp make_pair
+#define pb push_back
+#define popb pop_back
+#define mset(v, x) memset(v, x, sizeof(v))
+#define mset2d(v, x, n) memset(v, x, sizeof(v[0][0]) * n * n)
+#define mset2dd(v, x, n, m) memset(v, x, sizeof(v[0][0]) * n * m)
+#define debug(x) cout<<#x<<" : "<<x<<endl
+#define debug2(x,y) cout<<#x<<" : "<<x<<" & "<<#y<<" : "<<y<<endl
+#define test() cout<<"hola papa"<<endl
 #define MOD 1000000007
+#define MAX 101
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
 using namespace std;
 
-int v[MAX][2];
 int n, k, d;
 
-void suma(int &a, int b){
-	a += b;
-	if(a >= MOD) a -= MOD;
+ll sum(ll a, ll b) {
+	return (a % MOD + b % MOD) % MOD;
 }
 
-void dp(){
-	for(int i = 1; i <= n; ++i){
-		for(int j = 1; j <= k; ++j){
-			if(j > i) break;
-			if(j < d){
-				suma(v[i][0], v[i-j][0]);
-				suma(v[i][1], v[i-j][1]);
-			}else{
-				suma(v[i][1], v[i-j][0]);
-				suma(v[i][1], v[i-j][1]);
+ll dp[MAX][2];
+
+ll solve() {
+	mset2dd(dp, 0, MAX, 2);
+	dp[n][1] = 1, dp[n][0] = 0;
+	for (int i = n-1; i >= 0; --i) {
+		int aux = 0;
+		for (int j = i+1; j <= n && aux < k; ++j, ++aux) {
+			dp[i][1] = sum(dp[i][1], dp[j][1]);
+			if (j - i < d) {
+				dp[i][0] = sum(dp[i][0], dp[j][0]);
+			} else {
+				dp[i][0] = sum(dp[i][0], dp[j][1]);
 			}
 		}
 	}
+	return dp[0][0];
 }
 
-int main(){
-	while(cin>>n>>k>>d){
-		for(int i = 1; i<=n ; ++i) v[i][0] = v[i][1] = 0;
-		v[0][0] = 1;
-		dp();
-		cout<<v[n][1]<<endl;
+int main() {
+	ios::sync_with_stdio(false);
+	while (cin >> n >> k >> d) {
+		cout << solve() << "\n";
 	}
+	return 0;
 }
