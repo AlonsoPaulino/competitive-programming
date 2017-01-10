84e963321b93db50d4d0bd13bec5e5198d7cded5
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2017-01-09 21:03:10 -0500

    New Year Transportation solved

---
+++ Codeforces/New Year Transportation.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define pi acos(-1)
+#define sz(x) (int) x.size()
+#define pii pair<int, int>
+#define pill pair<int, long long>
+#define mii map<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define popb pop_back
+#define all(v) v.begin(),v.end()
+#define mod(x, m) ((x % m) + m) % m
+#define mset(v, x) memset(v, x, sizeof(v))
+#define mset2d(v, x, n) memset(v, x, sizeof(v[0][0]) * n * n)
+#define mset2dd(v, x, n, m) memset(v, x, sizeof(v[0][0]) * n * m)
+#define debug(x) cerr<<#x<<" : "<<x<<endl
+#define debug2(x,y) cerr<<#x<<" : "<<x<<" & "<<#y<<" : "<<y<<endl
+#define test() cerr<<"hola papa"<<endl
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+using namespace std;
+
+const int MAX = 30000;
+
+int n, t, x, k = 1;
+bool ans = 0;
+vi v(MAX + 5);
+
+int main() {
+    cin >> n >> t;
+    for (int i = 1; i < n; ++i) {
+        cin >> v[i];
+    }
+    while (k < t && !ans) {
+        k += v[k];
+        if (k == t) {
+            ans = 1;
+        }
+    }
+    cout << (ans ? "YES" : "NO") << "\n";
+}
