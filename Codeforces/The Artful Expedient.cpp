a592ef465609affa5bd6fe5f1128f9bfa773e8f2
Alonso <alonsopf1@gmail.com>
2018-05-28 22:27:59 -0500

    Solve The Artful Expedient problem

---
+++ Codeforces/The Artful Expedient.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define pi acos(-1)
+#define sz(x) (int) x.size()
+#define pii pair<int, int>
+#define pill pair<int, long long>
+#define mii map<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define popb pop_back
+#define all(v) v.begin(),v.end()
+#define mset(v, x) memset(v, x, sizeof(v))
+#define mset2d(v, x, n) memset(v, x, sizeof(v[0][0]) * n * n)
+#define mset2dd(v, x, n, m) memset(v, x, sizeof(v[0][0]) * n * m)
+#define debug(x) cerr<<#x<<" : "<<x<<endl
+#define debug2(x,y) cerr<<#x<<" : "<<x<<" & "<<#y<<" : "<<y<<endl
+#define test() cerr<<"hola papa"<<endl
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+using namespace std;
+
+int n, xi, yi, a;
+
+int main() {
+    cin >> n;
+
+    int t = 0;
+
+    vi vx(n), vy(n);
+    vb b(4000010, false);
+
+    for (int i = 0; i < 2 * n; ++i) {
+        cin >> a;
+        if (i < n) vx[i] = a; 
+        else vy[i % n] = a;
+
+        b[a] = true;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (b[vx[i] ^ vy[j]]) {
+                ++t;
+            }
+        }
+    }
+
+    cout << (t & 1 ? "Koyomi" : "Karen") << "\n";
+}
