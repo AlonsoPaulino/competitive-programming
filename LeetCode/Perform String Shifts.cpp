1368092d71f86100e0745a7d648661635e3f5cf5
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-05-23 23:14:28 -0500

    remove unneded cout in string shifts

--- LeetCode/Perform String Shifts.cpp
+++ LeetCode/Perform String Shifts.cpp
@@ -4,7 +4,6 @@ public:
         int l = 0, r = 0, n = (int) s.size();
         for (auto& x: shift) if (x[0] == 0) l+=x[1]; else r += x[1];
         int diff = abs(r - l) % n;
-        cout << diff << endl;
         if (r > l) {
             return s.substr(n - diff) + s.substr(0, n - diff);
         } else {
