ab14f23cf43c69302323122665d4ce140df2fcd7
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-05-24 19:57:06 -0500

    Merge BST from Preorder solution into one with both solutions

--- LeetCode/Construct Binary Search Tree from Preorder Traversal.cpp
+++ LeetCode/Construct Binary Search Tree from Preorder Traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// NOTE : This is the code I wrote the last time I submitted this problem and got AC :)
 class Solution {
 public:
     vector<int> v;
@@ -28,5 +29,42 @@ public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         v = preorder;
         return subTree(0, (int) preorder.size());
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ // This is the code I wrote the very first time I had to deal with this problem :)
+class Solution {
+public:
+    vector<int> v;
+    
+    TreeNode* subtree(int x, int n) {
+        if (n - x <= 0) return NULL;
+        TreeNode* root = new TreeNode(v[x]);
+        if (n - x == 1) return root;
+        int k = n;
+        for (int i = x + 1; i < n; ++i) {
+            if (v[i] > v[x]) {
+                k = i;
+                break;
+            }
+        }
+        root->left = subtree(x + 1, k);
+        root->right = subtree(k, n);
+        return root;
+    }
+    
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        v = preorder;
+        if (preorder.size() == 0) return NULL;
+        return subtree(0, v.size());
     }
 };
