8de4319cb86bc32d1c6e627782109b2a36e0a254
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-06-02 16:21:27 -0500

    Add New Theatre Square problem solution

---
+++ Codeforces/1359B - New Theatre Square.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define pi acos(-1)
+#define sz(x) (int) x.size()
+#define pii pair<int, int>
+#define pill pair<int, long long>
+#define mii map<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vs vector<string>
+#define vb vector<bool>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define popb pop_back
+#define all(v) v.begin(),v.end()
+#define mset(v, x) memset(v, x, sizeof(v))
+#define mset2d(v, x, n) memset(v, x, sizeof(v[0][0]) * n * n)
+#define mset2dd(v, x, n, m) memset(v, x, sizeof(v[0][0]) * n * m)
+#define debug(x) cerr<<#x<<" : "<<x<<endl
+#define debug2(x,y) cerr<<#x<<" : "<<x<<" & "<<#y<<" : "<<y<<endl
+#define test() cerr<<"hola papa"<<endl
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+using namespace std;
+
+int t, n, m, x, y;
+char c;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin >> t;
+    while (t--) {
+        cin >> n >> m >> x >> y;
+        y = min(y, x + x);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = 0; j < m; ++j) {
+                cin >> c;
+                if (c == '*') {
+                    ans += (cnt / 2) * y;
+                    if (cnt & 1) ans += x;
+                    cnt = 0;
+                } else cnt++;
+            }
+            ans += (cnt / 2) * y;
+            if (cnt & 1) ans += x;
+        }
+        cout << ans << endl;
+    }
+}
