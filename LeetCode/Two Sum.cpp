c7a52b17a78b58f9b8ee92630c5c77a1d657dcfd
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-06-13 20:13:24 -0500

    Improve Two Sums solution

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans(2);
-        map<int, int> m;
-        for (int i = 0; i < nums.size(); ++i) {
-            m[nums[i]] = i + 1;
+        unordered_map<int, int> m;
+        int n = (int) nums.size();
+        for (int i = 0; i < n; ++i) {
+            m[nums[i]] = i;
         }
-        for (int i = 0; i < nums.size(); ++i) {
-            int d = target - nums[i];
-            if (m[d] && ((m[d] - 1) != i)) {
+        vector<int> ans(2, 0);
+        for (int i = 0; i < n; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (m.find(y) != m.end() && i != m[y]) {
                 ans[0] = i;
-                ans[1] = m[d] - 1;
+                ans[1] = m[y];
                 break;
             }
         }
