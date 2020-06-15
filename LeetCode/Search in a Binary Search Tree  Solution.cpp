6ba7d73f4450ad33626ed02c772f79fe1aa7538f
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-06-15 12:56:04 -0500

    Add Search in a BST solution

---
+++ LeetCode/Search in a Binary Search Tree  Solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == NULL) return NULL;
+        
+        if (val < root->val) return searchBST(root->left, val);
+        if (val > root->val) return searchBST(root->right, val);
+        
+        return root;
+    }
+};
