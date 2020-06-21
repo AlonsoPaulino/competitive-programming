39c135e8f8d953569d377cdd932844f3b29c9709
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-06-20 20:22:22 -0500

    Add permutation sequence

---
+++ LeetCode/Permutation Sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s;
+        for (int i = 1; i <= n; ++i) {
+            s += (i + '0');
+        }
+        for (int i = 1; i < k; ++i) {
+            next_permutation(s.begin(), s.end());
+        }
+        return s;
+    }
+};
