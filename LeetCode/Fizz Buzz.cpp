06ea47fdf84625662e4747170572f84d9be5f891
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-08-26 16:14:00 -0500

    add Fizz Buzz solution

---
+++ LeetCode/Fizz Buzz.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for (int i = 1; i <= n; ++i) {
+            string s;
+            if (i % 3 == 0) s += "Fizz";
+            if (i % 5 == 0) s += "Buzz";
+            if ((int) s.size() == 0) s += to_string(i);
+            ans.push_back(s);
+        }
+        return ans;
+    }
+};
