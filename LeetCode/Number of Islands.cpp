2aad116385168d9590422d8b915bdec16a154035
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2020-12-29 13:42:24 -0500

    Add new solution to number of islands

--- LeetCode/Number of Islands.cpp
+++ LeetCode/Number of Islands.cpp
@@ -34,3 +34,48 @@ public:
         return ans;
     }
 };
+
+/* Another solution submitted by 12/29/2020 */
+class Solution {
+    int n, m;
+public:
+    bool isValid(int i, int j) {
+        return i >= 0 && j >= 0 && i < n && j < m;
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        n = (int) grid.size();
+        if (n == 0) return 0;
+        m = (int) grid[0].size();
+        
+        int ans = 0;
+        int di[] = { 0, 0, 1, -1 };
+        int dj[] = { 1, -1, 0, 0 };
+        
+        vector<vector<bool>> visit(n, vector<bool>(m, 0));
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                char x = grid[i][j];
+                if (!visit[i][j] && x == '1') {
+                    ++ans;
+                    stack<pair<int, int>> st;
+                    st.push({i, j});
+                    while (!st.empty()) {
+                        auto u = st.top(); st.pop();
+                        visit[u.first][u.second] = true;
+                        
+                        for (int k = 0; k < 4; ++k) {
+                            int ii = u.first + di[k], jj = u.second + dj[k];
+                            if (isValid(ii, jj) && grid[ii][jj] == '1' && !visit[ii][jj]) {
+                                st.push({ii, jj});
+                            }
+                        }
+                    }           
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
