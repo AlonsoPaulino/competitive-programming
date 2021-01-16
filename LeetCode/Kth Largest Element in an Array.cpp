4ef948a772230d2693222ac520e16ac88880d941
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-01-16 09:40:17 -0500

    Add another solution for Kth largest element in an array

--- LeetCode/Kth Largest Element in an Array.cpp
+++ LeetCode/Kth Largest Element in an Array.cpp
@@ -5,3 +5,16 @@ public:
         return nums[k - 1];
     }
 };
+
+// Another solution written in 01/16/2021
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = (int) nums.size();
+        
+        if (n == 0) return 0;
+        
+        sort(nums.begin(), nums.end());
+        return nums[n - k];
+    }
+};
