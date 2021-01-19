99b65e8e5deb1d8bf2a70c0b73faa33c3b83ac27
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-01-19 10:47:32 -0500

    Update Longest Palindromic Substring.cpp

--- LeetCode/Longest Palindromic Substring.cpp
+++ LeetCode/Longest Palindromic Substring.cpp
@@ -22,3 +22,35 @@ public:
         return ans;
     }
 };
+
+// Another solution written in 01/19/2021
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = (int) s.size();
+        
+        if (n == 0) return "";
+        
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        
+        int k = 0, l = 1;
+        
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i + j < n; ++i) {
+                int z = i + j;
+                
+                if (j == 0) dp[i][z] = true;
+                else if (j == 1) {
+                    dp[i][z] = s[i] == s[z];
+                } else {
+                    dp[i][z] = s[i] == s[z] && dp[i + 1][z - 1];
+                }
+                if (dp[i][z]) {
+                    l = j + 1, k = i;
+                }
+            }
+        }
+    
+        return s.substr(k, l);
+    }
+};
