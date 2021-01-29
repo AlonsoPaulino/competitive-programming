b18ca86e221263693c064e4a58a162bb4d9f4211
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-01-28 22:35:53 -0500

    Update Smallest String With A Given Numeric Value.cpp

--- LeetCode/Smallest String With A Given Numeric Value.cpp
+++ LeetCode/Smallest String With A Given Numeric Value.cpp
@@ -22,3 +22,21 @@ public:
         return ans;
     }
 };
+
+// Another solution written at 01/28/2021
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        int acum = 0;
+        string ans = "";
+        
+        for (int i = 0; i < n; ++i) {
+            int x = max(1, k - acum - 26 * (n - i - 1));
+            char c = 'a' + x - 1;
+            acum += x;
+            ans += c;
+        }
+        
+        return ans;
+    }
+};
