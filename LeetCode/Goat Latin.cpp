f6808f72bad288c712444fd39d594947792529d0
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-06-19 01:25:44 -0500

    Update Goat Latin.cpp

--- LeetCode/Goat Latin.cpp
+++ LeetCode/Goat Latin.cpp
@@ -31,3 +31,52 @@ public:
         return ans;
     }
 };
+
+// Another solution written at 06/19/2021
+class Solution {
+    const string ma = "ma";
+    char v[5] = { 'a', 'e', 'i', 'o', 'u' };
+public:
+    string process_word(string s, int i) {
+        string res = "";
+        
+        char f = tolower(s[0]);
+        bool isV = false;
+        for (int k = 0; !isV && k < 5; ++k) {
+            if (f == v[k]) isV = true;
+        }
+        
+        if (isV) {
+            res = s + ma;
+        } else {
+            res = s.substr(1, s.size() - 1);
+            res += s[0];
+            res += ma;
+        }
+        
+        for (int k = 0; k < i; ++k) {
+            res += "a";
+        }
+        return res;
+    }
+    
+    string toGoatLatin(string sentence) {
+        int n = sentence.size(), iw = 0;
+        string ans = "";
+        string s = "";
+        for (int i = 0; i < n; ++i) {
+            if (sentence[i] != ' ') {
+                s += sentence[i];
+            } else {
+                if (ans.size() > 0) ans += " ";
+                ans += process_word(s, ++iw);
+                s = "";
+            }
+        }
+        if (s.size() > 0) {
+            if (ans.size() > 0) ans += " ";
+            ans += process_word(s, ++iw);
+        }
+        return ans;
+    }
+};
