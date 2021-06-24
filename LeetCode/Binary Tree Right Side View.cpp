3beb8ae0755f1523ea7bb2a69c0580e30dac6752
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-06-24 00:45:39 -0500

    Create Binary Tree Right Side View.cpp

---
+++ LeetCode/Binary Tree Right Side View.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        
+        if (root == NULL) {
+            return ans;
+        }
+        
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        
+        while (!q.empty()) {
+            auto u = q.front(); q.pop();
+            auto node = u.first;
+            int depth = u.second;
+            
+            if (ans.size() <= depth) {
+                ans.push_back(node->val);
+            } else {
+                ans[depth] = node->val;
+            }
+            
+            if (node->left) q.push({node->left, depth + 1});
+            if (node->right) q.push({node->right, depth + 1});
+        }
+        
+        return ans;
+    }
+};
