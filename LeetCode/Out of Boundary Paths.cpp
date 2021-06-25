e16942bc98280a2293bb7662f0d70aabd87c21bf
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-06-25 01:31:01 -0500

    Update Out of Boundary Paths.cpp

--- LeetCode/Out of Boundary Paths.cpp
+++ LeetCode/Out of Boundary Paths.cpp
@@ -6,7 +6,7 @@ const int dj[4] = { 1, -1, 0, 0 };
 
 class Solution {
     int dp[MAX][MAX][MAX];
-    int r = 0, c = 0;
+    int r = 0, c = 0, ans = 0;
 public:
     int sum(int a, int b) {
         return (a % MOD + b % MOD) % MOD;
@@ -15,23 +15,7 @@ public:
     int is_valid(int i, int j) {
         return i >= 0 && i < r && j >= 0 && j < c;
     }
-    
-    void solve(int i, int j, int moves) {
-        for (int i = 0; i < MAX; ++i) {
-            for (int j = 0; j < MAX; ++j) {
-                if (dp[i][j][moves - 1] > 0) {
-                    for (int k = 0; k < 4; ++k) {
-                        int ii = i + di[k], jj = j + dj[k];
-                        if (is_valid(ii, jj)) {
-                            dp[ii][jj][moves] = 
-                                sum(dp[ii][jj][moves], dp[i][j][moves - 1]);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
+
     int findPaths(int m, int n, int max_move, int x, int y) {
         fill_n(&dp[0][0][0], sizeof(dp) / sizeof(dp[0][0][0]), 0);
         r = m, c = n;
@@ -39,20 +23,27 @@ public:
         dp[x][y][0] = 1;
         
         for (int m = 1; m < max_move; ++m) {
-            solve(x, y, m);
-        }
-        
-        int ans = 0;
-        
-        for (int i = 0; i < r; ++i) {
-            for (int m = 0; m < max_move; ++m) {
-                ans = sum(ans, dp[i][0][m]);
-                ans = sum(ans, dp[i][c - 1][m]);
+            for (int i = 0; i < r; ++i) {
+                for (int j = 0; j < c; ++j) {
+                    if (dp[i][j][m - 1] > 0) {
+                        for (int k = 0; k < 4; ++k) {
+                            int ii = i + di[k], jj = j + dj[k];
+                            if (is_valid(ii, jj)) {
+                                dp[ii][jj][m] = 
+                                    sum(dp[ii][jj][m], dp[i][j][m - 1]);
+                            }
+                        }
+                    }
+                }
             }
         }
         
-        for (int j = 0; j < c; ++j) {
-            for (int m = 0; m < max_move; ++m) {
+        for (int m = 0; m < max_move; ++m) {
+            for (int i = 0; i < r; ++i) {
+                ans = sum(ans, dp[i][0][m]);
+                ans = sum(ans, dp[i][c - 1][m]);
+            }
+            for (int j = 0; j < c; ++j) {
                 ans = sum(ans, dp[0][j][m]);
                 ans = sum(ans, dp[r - 1][j][m]);
             }
