a6b8194b6f2a2a24ec9513f09a377f438f9d1652
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-07-01 02:12:28 -0500

    Update Lowest Common Ancestor of a Binary Tree.cpp

--- LeetCode/Lowest Common Ancestor of a Binary Tree.cpp
+++ LeetCode/Lowest Common Ancestor of a Binary Tree.cpp
@@ -41,3 +41,36 @@ public:
         return p;
     }
 };
+
+// Another solution written at 07/01/2021
+class Solution {
+    unordered_map<int, int> depth;
+    unordered_map<int, TreeNode*> parent;
+public:
+    void dfs(TreeNode* p, TreeNode* x, int d) {
+        if (x == NULL) return;
+        parent[x->val] = p;
+        depth[x->val] = d;
+        if (x->left) dfs(x, x->left, d + 1);
+        if (x->right) dfs(x, x->right, d + 1);
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) return NULL;
+        dfs(root, root, 0);
+        TreeNode *x, *y;
+        if (depth[p->val] > depth[q->val]) {
+            x = p, y = q;
+        } else {
+            y = p, x = q;
+        }
+        while (depth[x->val] > depth[y->val]) {
+            x = parent[x->val];
+        }
+        while (x->val != y->val) {
+            x = parent[x->val];
+            y = parent[y->val];
+        }
+        return x;
+    }
+};
