5f8b0807ae49fe3f2c99d8b019ccfe4624f15c7a
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-07-12 19:14:42 -0500

    Update Isomorphic Strings.cpp

--- LeetCode/Isomorphic Strings.cpp
+++ LeetCode/Isomorphic Strings.cpp
@@ -15,3 +15,28 @@ public:
         return true;
     }
 };
+
+// Another solution written on 07/12/2021
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> sm, tm;
+        int n = (int) s.size(); // s.lenght == t.length
+        
+        for (int i = 0; i < n; ++i) {
+            char cs = s[i], ct = t[i];
+            auto its = sm.find(cs), itt = tm.find(ct);
+            if (its == sm.end() && itt != tm.end()) return false;
+            if (its != sm.end() && itt == tm.end()) return false;
+            if (its == sm.end() && itt == tm.end()) {
+                sm[cs] = ct, tm[ct] = cs;
+                continue;
+            }
+            if (its->second != ct || itt->second != cs) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
