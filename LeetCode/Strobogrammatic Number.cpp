d86e7ce798a49970aaeb782358a6ee2327eb4dc0
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-08-04 02:56:01 -0500

    Update Strobogrammatic Number.cpp

--- LeetCode/Strobogrammatic Number.cpp
+++ LeetCode/Strobogrammatic Number.cpp
@@ -26,3 +26,32 @@ public:
         return true;
     }
 };
+
+// Another solution written on 07/04/2021
+class Solution {
+public:
+    bool check_1(int a) {
+        return a == 1 || a == 0 || a == 8;
+    }
+    
+    bool check_2(int a, int b) {
+        if (a == b && check_1(a)) return true;
+        if (a == 6 && b == 9) return true;
+        return a == 9 && b == 6;
+    }
+    
+    bool isStrobogrammatic(string num) {
+        int n = (int) num.size();
+        for (int i = 0; i < n / 2; ++i) {
+            int j = n - i - 1;
+            if (!check_2(num[i] - '0', num[j] - '0')) {
+                return false;
+            }    
+        }
+        if (n & 1) {
+            int x = num[n / 2] - '0';
+            return check_1(x);
+        }
+        return true;
+    }
+};
