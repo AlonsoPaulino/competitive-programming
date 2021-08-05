c343446bad8b156382234f72829d43d1bb195373
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-08-05 02:10:43 -0500

    Update Queue Reconstruction by Height.cpp

--- LeetCode/Queue Reconstruction by Height.cpp
+++ LeetCode/Queue Reconstruction by Height.cpp
@@ -12,3 +12,21 @@ public:
         return ans;
     }
 };
+
+// Another solution written on 08/05/2021
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int> &b) {
+            if (a[0] != b[0]) return a[0] > b[0];
+            return a[1] < b[1];
+        });
+        int n = (int) people.size();
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; ++i) {
+            auto x = people[i];
+            ans.insert(ans.begin() + x[1], x);
+        }
+        return ans;
+    }
+};
