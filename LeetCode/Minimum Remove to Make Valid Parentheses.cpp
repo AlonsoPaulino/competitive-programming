2c025adff7528d210a7e5939b110238ffa8aa046
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-08-26 02:04:35 -0500

    Update Minimum Remove to Make Valid Parentheses.cpp

--- LeetCode/Minimum Remove to Make Valid Parentheses.cpp
+++ LeetCode/Minimum Remove to Make Valid Parentheses.cpp
@@ -30,3 +30,41 @@ public:
         return ans;
     }
 };
+
+// Another solution written on 08/26/2021
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        vector<int> pos;
+        int n = (int) s.size();
+        stack<int> left;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(') {
+                left.push(i);
+            } else if (s[i] == ')') {
+                if (!left.empty()) {
+                    left.pop();
+                } else {
+                    pos.push_back(i);
+                }
+            }
+        }
+        while (!left.empty()) {
+            pos.push_back(left.top());
+            left.pop();
+        }
+        sort(pos.begin(), pos.end());
+        string ans = "";
+        int m = (int) pos.size(), j = 0;
+        for (int i = 0; i < n; ++i) {
+            if (j < m) {
+                if (pos[j] == i) {
+                    ++j;
+                    continue;
+                }
+            }
+            ans += s[i];
+        }
+        return ans;
+    }
+};
