60a1843768e228ba36e2cb0b5141d57fda711f4d
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2022-01-21 00:10:41 -0500

    Update Read N Characters Given Read4 II - Call multiple times.cpp

--- LeetCode/Read N Characters Given Read4 II - Call multiple times.cpp
+++ LeetCode/Read N Characters Given Read4 II - Call multiple times.cpp
@@ -26,3 +26,33 @@ public:
         return i;
     }
 };
+
+
+// Another solution written on 01/21/2022
+class Solution {
+public:
+    
+    int p = 0, sz = 0;
+    char *buf4 = new char[4];
+    
+    int read(char *buf, int n) {
+        int cnt = n;
+        int x = 0;
+        while (p < sz && cnt > 0) {
+            buf[x++] = buf4[p++];
+            cnt = cnt - 1;
+        }
+        while (cnt > 0) {
+            sz = read4(buf4);
+            if (sz == 0) {
+                break;
+            }
+            for (int i = 0; i < sz && cnt > 0; ++i) {
+                buf[x++] = buf4[i];
+                cnt = cnt - 1;
+                p = i + 1;
+            }
+        }
+        return n - cnt;
+    }
+};
