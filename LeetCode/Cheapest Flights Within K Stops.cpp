6d4b66c30b91ae758da3eab65f7ee4c349c1080f
Luis Alonso Paulino <alonsopf1@gmail.com>
2023-01-27 14:23:40 +0000

    Update Cheapest Flights Within K Stops.cpp

--- LeetCode/Cheapest Flights Within K Stops.cpp
+++ LeetCode/Cheapest Flights Within K Stops.cpp
@@ -38,3 +38,39 @@ public:
         return ans != INF ? ans : -1;
     }
 };
+
+// New solution written on Jan 27, 2023
+class Solution {
+public:
+    const int INF = 1e9;
+
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<vector<int>>> g(n + 5, vector<vector<int>>(n + 5, vector<int>(k + 5, INF)));
+        for (auto &f: flights) {
+            g[f[0]][f[1]][0] = f[2];
+        }
+
+        int result = g[src][dst][0];
+
+        for (int t = 1; t <= k; ++t) {
+            for (int j = 0; j < n; ++j) {
+                int dist = INF;
+                for (int x = 0; x < n; ++x) {
+                    if (x != j && x != src) {
+                        dist = min(dist, g[src][x][t - 1] + g[x][j][0]);
+                    }
+                }
+
+                g[src][j][t] = dist;
+                if (j == dst) {
+                    result = min(result, g[src][j][t]);
+                }
+            }
+        }
+
+        if (result >= INF) {
+            return -1;
+        }
+        return result;
+    }
+};
